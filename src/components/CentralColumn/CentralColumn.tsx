9877eb5b0a0e65177a7545418c78f324dd092a15
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-05-05 15:34:52 +0300

    feat: implement central column layout

--- src/components/CentralColumn/CentralColumn.tsx
+++ src/components/CentralColumn/CentralColumn.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './CentralColumn.scss';
 
 function CentralColumn() {
-  return <div className={'central-column'} />;
+  return (
+    <div className={'central-column'}>
+      <div className="hexagon">
+        <div className="hexTop" />
+        <div className="hexBottom" />
+      </div>
+      <div className={'central-column__triangle'} />
+      <div className={'central-column__block'} />
+    </div>
+  );
 }
 
 export default CentralColumn;
