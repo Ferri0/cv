fe420d9c6f5280bc8c0f27fb348a167810a4680d
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-05-05 22:01:22 +0300

    fix: projectBlock layout

--- src/components/ProjectBlock/ProjectBlock.tsx
+++ src/components/ProjectBlock/ProjectBlock.tsx
@@ -4,7 +4,8 @@ import './ProjectBlock.scss';
 type projectDataType = {
   data: {
     project: string;
-    link: string;
+    linkText: string;
+    url: string;
     description: string;
   };
 };
@@ -13,7 +14,13 @@ function ProjectBlock({data}: projectDataType) {
   return (
     <div key={data.project} className={'project-block'}>
       <div className={'project-block__project-title'}>{data.project}</div>
-      <div className={'project-block__link'}>{data.link}</div>
+      <a
+        href={data.url}
+        target={'_blank'}
+        className={'project-block__link'}
+        rel="noreferrer">
+        {data.linkText}
+      </a>
       <div className={'project-block__description'}>{data.description}</div>
     </div>
   );
