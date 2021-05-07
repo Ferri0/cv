f67427a36d1f943652602acdc6b4eaed7d1ae424
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-05-07 22:43:17 +0300

    feat: complete SkillsSection

--- src/components/HobbySection/HobbySection.tsx
+++ src/components/HobbySection/HobbySection.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './HobbySection.scss';
 import {RootStateOrAny, useSelector} from 'react-redux';
-import testImage from '../../assets/IMG_2994_1000x1000.png';
+import hobby1 from '../../assets/icons/hobbies/game-controller.svg';
+import hobby2 from '../../assets/icons/hobbies/book.svg';
+import hobby3 from '../../assets/icons/hobbies/headphones.svg';
+import hobby4 from '../../assets/icons/hobbies/steering-wheel.svg';
 import * as hobbyText from '../../data/hobbySection.json';
 
 function HobbySection() {
@@ -20,10 +23,10 @@ function HobbySection() {
         {hobbyText.hobbySectionTitle[lang]}
       </div>
       <div className={'hobby-section__icons-block'}>
-        <img src={testImage} alt="hobby 1" className={'hobby-section__icon'} />
-        <img src={testImage} alt="hobby 2" className={'hobby-section__icon'} />
-        <img src={testImage} alt="hobby 3" className={'hobby-section__icon'} />
-        <img src={testImage} alt="hobby 4" className={'hobby-section__icon'} />
+        <img src={hobby1} alt="hobby 1" className={'hobby-section__icon'} />
+        <img src={hobby2} alt="hobby 2" className={'hobby-section__icon'} />
+        <img src={hobby3} alt="hobby 3" className={'hobby-section__icon'} />
+        <img src={hobby4} alt="hobby 4" className={'hobby-section__icon'} />
       </div>
     </section>
   );
