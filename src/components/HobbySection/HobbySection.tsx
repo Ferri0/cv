2994c5d2c5179e99edd159c604a7bff65ac33f81
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-05-08 22:53:45 +0300

    feat: HobbySection themes

--- src/components/HobbySection/HobbySection.tsx
+++ src/components/HobbySection/HobbySection.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './HobbySection.scss';
 import {RootStateOrAny, useSelector} from 'react-redux';
-import hobby1 from '../../assets/icons/hobbies/game-controller.svg';
-import hobby2 from '../../assets/icons/hobbies/book.svg';
-import hobby3 from '../../assets/icons/hobbies/headphones.svg';
-import hobby4 from '../../assets/icons/hobbies/steering-wheel.svg';
+import hobby1_dark from '../../assets/icons/hobbies/game-controller.svg';
+import hobby1_light from '../../assets/icons/hobbies/game-controller_light.svg';
+import hobby2_dark from '../../assets/icons/hobbies/book.svg';
+import hobby2_light from '../../assets/icons/hobbies/book_light.svg';
+import hobby3_dark from '../../assets/icons/hobbies/headphones.svg';
+import hobby3_light from '../../assets/icons/hobbies/headphones_light.svg';
+import hobby4_dark from '../../assets/icons/hobbies/steering-wheel.svg';
+import hobby4_light from '../../assets/icons/hobbies/steering-wheel_light.svg';
 import * as hobbyText from '../../data/hobbySection.json';
 
 function HobbySection() {
@@ -24,10 +28,26 @@ function HobbySection() {
       </div>
       <div
         className={`hobby-section__icons-block hobby-section__icons-block--${theme}`}>
-        <img src={hobby1} alt="hobby 1" className={'hobby-section__icon'} />
-        <img src={hobby2} alt="hobby 2" className={'hobby-section__icon'} />
-        <img src={hobby3} alt="hobby 3" className={'hobby-section__icon'} />
-        <img src={hobby4} alt="hobby 4" className={'hobby-section__icon'} />
+        <img
+          src={theme === 'dark' ? hobby1_dark : hobby1_light}
+          alt="hobby 1"
+          className={'hobby-section__icon'}
+        />
+        <img
+          src={theme === 'dark' ? hobby2_dark : hobby2_light}
+          alt="hobby 2"
+          className={'hobby-section__icon'}
+        />
+        <img
+          src={theme === 'dark' ? hobby3_dark : hobby3_light}
+          alt="hobby 3"
+          className={'hobby-section__icon'}
+        />
+        <img
+          src={theme === 'dark' ? hobby4_dark : hobby4_light}
+          alt="hobby 4"
+          className={'hobby-section__icon'}
+        />
       </div>
     </section>
   );
